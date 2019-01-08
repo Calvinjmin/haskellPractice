d3570d3f4224d9ed6ab40ca49a227f9efe1d36ee
Calvin Min <MinCJ19@pwcs-edu.org>
2019-01-08 13:52:20 -0500

    haskell try 2

--- manipulation.hs
+++ manipulation.hs
@@ -1,3 +1,5 @@
 example1 str1 int1 = if mod int1 2 == 0 then str1 else reverse str1
 
 example2 xs = [ if even y then xs !! y else reverse (xs !! y ) | y <- [0..length xs-1] ]   
+
+trial xs = [ splitAt x xs | x <- [20,40..length xs] ] 
